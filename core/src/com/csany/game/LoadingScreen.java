f7d0d2bddd56fde8b70f5925c70b2bb348092a57
tuskeb <tuskebganz@freemail.hu>
2016-01-29 16:39:05 +0100

    Még most is

--- core/src/com/csany/game/LoadingScreen.java
+++ core/src/com/csany/game/LoadingScreen.java
@@ -21,10 +21,6 @@ import javafx.scene.Camera;
 public class LoadingScreen extends MyScreen {
 
     Stage stage;
-    float frameChanger;
-    int aa;
-    boolean teszt=false;
-
     Array<TextureAtlas.AtlasRegion> loadingAtlasRegions;
     public LoadingScreen() {
         setBackgroundColor(0f, 0f, 0f);
@@ -38,37 +34,13 @@ public class LoadingScreen extends MyScreen {
     public void show() {
         Assets.manager.load(Assets.LOADING_ATLAS);
         Assets.manager.finishLoading();
+
         loadingAtlasRegions = Assets.manager.get(Assets.LOADING_ATLAS).getRegions();
         sprite.setSize(loadingAtlasRegions.get(0).getRegionWidth(), loadingAtlasRegions.get(0).getRegionHeight());
         sprite.setPosition(MyScreen.WORLD_WIDTH/2-sprite.getWidth()/2,MyScreen.WORLD_HEIGHT/2-sprite.getHeight()/2);
         camera = new OrthographicCamera(1024,768);
         camera.translate(512,384);
         viewport = new ExtendViewport(1024, 768, camera);
-		stage = new Stage(viewport) {
-
-			final Array<TextureAtlas.AtlasRegion> loadingAtlasRegions = Assets.manager.get(Assets.LOADING_ATLAS).getRegions();
-
-
-			Sprite sprite = new Sprite();
-			{
-				sprite.setSize(400,400);
-				sprite.setPosition(312, 184);
-			}
-			@Override
-			public void draw() {
-				super.draw();
-
-				sprite.draw(batch);
-
-			}
-
-			@Override
-			public void act() {
-
-				int i = (int)(loadingAtlasRegions.size * Assets.manager.getProgress()) - 1;
-				sprite.setRegion(loadingAtlasRegions.get(Math.max(0, i)));
-			}
-		};
 
         Assets.load();
 
@@ -76,78 +48,26 @@ public class LoadingScreen extends MyScreen {
 
     @Override
     public void render(float delta) {
-        //super.render(delta);
-        Gdx.gl.glClearColor(r, g, b, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        super.render(delta);
 
-        frameChanger+=delta;
 
-        //stage.act();
+        if (Assets.manager.update()) {
+            Assets.afterLoaded();
+            ((MyGame) Gdx.app.getApplicationListener())
+                    .setScreen(new MenuScreen());
+        }
+
         batch.begin();
-        if (!teszt) {
-            sprite.setRegion(loadingAtlasRegions.get(0));
-            teszt = true;
-        }
-        int i = (int) (((float)loadingAtlasRegions.size * Assets.manager.getProgress()) - 1);
-        try {
-            if (frameChanger > 1.115) {
-                aa++;
-                sprite.setRegion(loadingAtlasRegions.get(aa));
-                frameChanger = 0;
-            }
-        }
-        catch (Exception e){
-            if (Assets.manager.update()) {
-                Assets.afterLoaded();
-                ((MyGame) Gdx.app.getApplicationListener())
-                        .setScreen(new MenuScreen());
-            }
-        }
+
+        final int i = (int) (((float)loadingAtlasRegions.size * Assets.manager.getProgress()) - 1);
+        sprite.setRegion(loadingAtlasRegions.get(Math.max(0, i)));
+
         sprite.draw(batch);
-        //stage.draw();
+
         batch.end();
 
         Assets.load();
     }
-
-    /*public void show() {
-		Assets.manager.load(Assets.LOADING_ATLAS);
-	    Assets.manager.finishLoading();
-		loadingAtlasRegions = Assets.manager.get(Assets.LOADING_ATLAS).getRegions();
-		sprite.setSize(loadingAtlasRegions.get(0).getRegionWidth(), loadingAtlasRegions.get(0).getRegionHeight());
-		sprite.setPosition(MyScreen.WORLD_WIDTH/2-sprite.getWidth()/2,MyScreen.WORLD_HEIGHT/2-sprite.getHeight()/2);
-        /*camera = new OrthographicCamera(1024,768);
-        camera.translate(512,384);
-        viewport = new ExtendViewport(1024, 768, camera);
-		stage = new Stage(viewport) {
-
-			final Array<TextureAtlas.AtlasRegion> loadingAtlasRegions = Assets.manager.get(Assets.LOADING_ATLAS).getRegions();
-
-
-			Sprite sprite = new Sprite();
-			{
-				sprite.setSize(400,400);
-				sprite.setPosition(312, 184);
-			}
-			@Override
-			public void draw() {
-				super.draw();
-
-				sprite.draw(batch);
-
-			}
-
-			@Override
-			public void act() {
-
-				int i = (int)(loadingAtlasRegions.size * Assets.manager.getProgress()) - 1;
-				sprite.setRegion(loadingAtlasRegions.get(Math.max(0, i)));
-			}
-		};
-
-    Assets.load();
-
-} */
 
     @Override
     public void hide() {
