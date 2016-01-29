fce2b150b48f35dc7c76f2c1393fbf9ed6d5c517
tuskeb <tuskebganz@freemail.hu>
2016-01-29 08:46:46 +0100

    Initial commit

---
+++ android/src/com/csany/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.csany.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.csany.game.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
